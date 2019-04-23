0830729744bb06acce5d7a956fc63d249447f63b
Eudris Cabrera <ecabrera@hacienda.gov.do>
2019-04-23 09:39:21 -0400

    Publishing an update to my website

---
+++ js/JSON-translation-data.js
@@ -0,0 +1,170 @@
+var linguJSON = {
+    "languages": [
+        {
+            "lang_name": "English",
+            "lang_code": "en",
+            "url_pattern": "?"
+        },
+        {
+            "lang_name": "Español",
+            "lang_code": "es",
+            "url_pattern": "?"
+        }
+    ],
+    "custom_lang_switcher_html": "<div id=\"langswitcher\" class=\"notranslate\"><a id=\"currlanglink\" class=\"btn-link\" href=\"\">[[linguCurrLang]]</a> <a href=\"[[linguTargetLangHref1]]\" class=\"targetlang  btn-link\">[[linguTargetLang1]]</a></div>",
+     "custom_lang_switcher_container_id": "langlinks",
+    "translated_segments": [
+        {
+            "source": "Home",
+            "target": "Portada"
+        },
+        {
+            "source": "Speakers",
+            "target": "Conferencistas",
+        },
+        {
+            "source": "Code of conduct",
+            "target": "Código de conducta",
+        },
+        {
+            "source": "Sponsors",
+            "target": "Patrocinadores",
+        },
+        {
+            "source": "About",
+            "target": "Acerca de",
+        },
+        {
+            "source": "Saturday, June 29, 2019. PUCMM, Santiago de los Caballeros. Dominican Republic.",
+            "target": " Sábado 29 de Junio del 2019. PUCMM, Santiago de los Caballeros. República Dominicana.",
+        },
+        {
+            "source": "Featured Speakers",
+            "target": "Conferencistas Destacados",
+        },
+        {
+            "source": "Contact Us",
+            "target": "Contáctanos",
+        },
+        {
+            "source": "About us",
+            "target": "Sobre Nosotros",
+        },
+        {
+            "source": "Organizers",
+            "target": "Organizadores",
+        },
+        {
+            "source": "JConf Dominicana 2019 is organized by the following organizations and developer communities",
+            "target": "JConf Dominicana 2019 está organizado por las siguientes organizaciones y comunidades de desarrolladores",
+        },
+        {
+            "source": "Santiago de los Caballeros, Dominican Republic.",
+            "target": "Santiago de los Caballeros, República Dominicana.",
+        },
+        {
+            "source": "Where",
+            "target": "¿Dónde?",
+        },
+        {
+            "source": "When",
+            "target": "¿Cuándo?",
+        },
+        {
+            "source": "PUCMM, Santiago de los Caballeros. Dominican Republic.",
+            "target": "PUCMM,Santiago de los Caballeros, República Dominicana.",
+        },
+        {
+            "source": "All Speakers",
+            "target": "Todos los Conferencistas",
+        },
+        {
+            "source": "Buy your Ticket!",
+            "target": "¡Adquiere tu entrada!",
+        },
+        {
+            "source": "Spring Developer Advocate @ Pivotal - USA",
+            "target": "Spring Developer Advocate @ Pivotal - USA",
+        },
+        {
+            "source": "Spring Developer & Advocate @ Pivotal - USA",
+            "target": "Spring Developer & Advocate @ Pivotal - USA",
+        },
+        {
+            "source": "Lead Java Developer Advocate @ IBM - Germany",
+            "target": "Lead Java Developer Advocate @ IBM - Germany",
+        },
+        {
+            "source": "Director and Chairperson @ Java Community Process (JCP) - USA",
+            "target": "Directora y Presidenta del Java Community Process (JCP) - USA",
+        },
+        {
+            "source": "Director of Developer Experience @ Red Hat - USA",
+            "target": "Director de Experiencia de Desarrollador en Red Hat - USA",
+        },
+        {
+            "source": "Software Engineer @ Produactivity - Guatemala",
+            "target": "Ingeniera de Software en Produactivity - Guatemala",
+        },
+        {
+            "source": "Newsletter",
+            "target": "Boletín",
+        },
+        {
+            "source": "Sign up and stay tuned for more information about this great event.",
+            "target": "Regístrese y permanezca en contacto para obtener más información sobre este gran evento.",
+        },
+        {
+            "source": "Enter your Email",
+            "target": "Introducir tu correo electrónico",
+        },
+        {
+            "source": "Please contact us if you are interested in becoming a JConf Dominicana sponsor or supporter.",
+            "target": "Póngase en contacto con nosotros si está interesado en convertirse en patrocinador o soporte de JConf Dominicana.",
+        },
+        {
+            "source": "Useful Links",
+            "target": "Enlaces de Interés",
+        },
+        {
+            "source": "Site map",
+            "target": "Mapa del sitio",
+        },
+        {
+            "source": "Buy Tickets",
+            "target": "Adquirir Entradas",
+        },
+        {
+            "source": "Tickets will be available soon. Sign up now to be notified when registration opens.",
+            "target": "Las entradas estarán disponibles pronto. Regístrese ahora para recibir una notificación cuando se abra el registro.",
+        },
+        {
+            "source": "Here are some of our speakers",
+            "target": "Algunos de nuestros conferencistas",
+        },
+        {
+            "source": "JConfDominicana is a community-driven conference organized by the Dominican Republic Java User Group (Aka JavaDominicano). It is designed to discuss the latest about the Java ecosystem, including alternatives JVM languages, Web and Mobile development, and newest Cloud tendencies.The conference offers a unique networking opportunity for developers, tech companies, and startups and a chance to enjoy the pristine beaches, cloud-forest treks, and the welcoming villages that the island has to offer. Come to the Dominican Republic to learn about latest Java technologies and have fun sightseeing.",
+            "target": "La JConf Dominicana es una conferencia de Tecnologías Java sin ánimo de lucro organizada por las comunidades de usuarios y desarrolladores de República Dominicana. Ha sido diseñada para ayudar a usuarios, desarrolladores, emprendedores y empresas a conocer las nuevas tecnológicas relacionadas con el ecosistema Java, incluyendo alternativas de lenguajes en el JVM, desarrollo Web y Móvil y  las nuevas tendencias en la nube. Además de los temas técnicos, la conferencia ofrece una oportunidad única para socializar con desarrolladores, empresas de tecnologías, emprendedores y disfrutar de las playas, de los bosques y los pueblos acogedores que ofrece la isla. Ven a la República Dominicana para conocer las últimas tecnologías de Java y divertirte haciendo turismo.",
+        },
+        {
+            "source": "Subscribe",
+            "target": "Suscribirse",
+        },
+        {
+            "source": "Click here",
+            "target": "Pulsar aquí",
+        },
+        {
+            "source": "JConfDominicana is a community-driven conference organized by the Dominican Republic Java User Group (Aka JavaDominicano). It is designed to discuss the latest about the Java ecosystem, including alternatives JVM languages, Web and Mobile development, and newest Cloud tendencies.",
+            "target": "La JConf Dominicana es una conferencia de Tecnologías Java sin ánimo de lucro organizada por las comunidades de usuarios y desarrolladores de República Dominicana. Ha sido diseñada para ayudar a usuarios, desarrolladores, emprendedores y empresas a conocer las nuevas tecnológicas relacionadas con el ecosistema Java, incluyendo alternativas de lenguajes en el JVM, desarrollo Web y Móvil y las nuevas tendencias en la nube.",
+        },
+        {
+            "source": "%D Days | %-H Hours | %M Minutes | %S Seconds",
+            "target": "%D Días | %-H Horas | %M Minutos | %S Segundos",
+        },
+        {
+            "source": "Tickets",
+            "target": "Inscripciones",
+        },
+    ]
+};
