ad33d67fa70afbceaaf85f3e46b7e7c6628990a2
Eudris Cabrera <eudris@gmail.com>
2025-04-28 01:20:38 -0400

    adding new speakers and sessions

--- src/jbake/assets/js/session.js
+++ src/jbake/assets/js/session.js
@@ -1,66 +1,12 @@
 import { getUsefulContents, getUsefulLink } from '/js/util-url.js';
-
-var fetchUrl = getUsefulContents("lang", "../json/sessions");
-
-fetch(fetchUrl)
-    .then(function (response) {
-        if (!response.ok) {
-            throw Error(response.statusText);
-        }
-
-        return response.json();
-    })
-    .then(function (sessionsJson) {
-
-      var sessionList = [];
-      for(let y in sessionsJson){
-        sessionList.push(sessionsJson[y]);
-      }
-
-      sessionList.sort(function(a, b){return a.id-b.id});
-
-      let filteredSessions = sessionList.filter((session) => {
-
-        return session.talk_format.indexOf("Workshop")==-1;
-    });
-
-      let sessions = document.getElementById('sessionsList');
-
-      filteredSessions.forEach((session) => processSession(session, sessions));
-
-});
-
-async function processSession(session, sessions){
-    let title = session.title;
-    let abstract = session.abstract;
-    let speakerId = session.speakers[0];
-
-    let time = session.time;
-
-  let speaker = await getSpeakerById(getUsefulContents("lang", "../json/speakers/"+speakerId));
-
-  sessions.innerHTML += createSessionCard(title,abstract,speaker, time);
-
-}
-
-$(document).on('click', '#sessionsList li.meeta-event-accordion-item > .meeta-event-accordion-toggle', function(){
-  if ($(this).hasClass("active")) {
-    $(this).removeClass("active");
-    $(this).siblings(".meeta-event-accordion-body").slideUp(200);
-} else {           
-    $(".meeta-event-accordion-item > .meeta-event-accordion-toggle").removeClass("active");
-    $(this).addClass("active");
-    $(".meeta-event-accordion-body").slideUp(200);
-    $(this).siblings(".meeta-event-accordion-body").slideDown(200);
-}
-});
+import {fetchData} from '/js/fetch-util.js';
 
 
 function createSessionCard(title,abstract, speaker,time) {
 
-  var speakerUrlDetail =  getUsefulLink("lang", "speaker-details.html?id=" + speaker.id);
+   let speakerUrlDetail =  getUsefulLink("lang", "speaker-details.html?id=" + speaker.speakerId);
 
-     var sessionHtml = "<li class=\"meeta-event-accordion-item\">"+
+     let sessionHtml = "<li class=\"meeta-event-accordion-item\">"+
      "<h3 class=\"meeta-event-accordion-toggle\">"+
      "<div class=\"image\">"+
      "<a href=\"" + speakerUrlDetail + "\"><img src=\"" +speaker.photoUrl +"\" alt=\""+speaker.name+"\"></a>"+
@@ -75,7 +21,6 @@ function createSessionCard(title,abstract, speaker,time) {
       "</div>"+
       "</li>";
 
-
         return sessionHtml;
 }
 
@@ -85,5 +30,55 @@ async function getSpeakerById(fetchUrlSpeaker){
     throw Error(response.statusText);
   }
   return response.json();
-      
 }
+
+async function processSession(session, sessions){
+    let title = session.title;
+    let abstract = session.abstract;
+    let speakerId = session.speakers[0];
+
+    let time = session.time;
+
+    let speaker = await getSpeakerById(getUsefulContents("lang", "../json/speakers/"+speakerId));
+
+    sessions.innerHTML += createSessionCard(title,abstract,speaker, time);
+}
+
+
+let filterConfirmedSessions = function(sessionsJson) {
+
+      let sessionList = [];
+
+      sessionsJson.forEach(sessionObj => sessionList.push(sessionObj));
+
+      sessionList.sort(function(a, b){return a.id-b.id});
+
+      let filteredSessions = sessionList.filter((session) => {
+            return session.talk_format.indexOf("Workshop")==-1;
+      });
+
+      return filteredSessions;
+}
+
+let renderSessionList = function(sessionJson) {
+      let sessions = document.getElementById('sessionsList');
+
+      sessionJson.forEach((session) => processSession(session, sessions));
+}
+
+let sessionUrl = getUsefulContents("lang", "../json/sessions");
+let jsonData = await fetchData(sessionUrl);
+let filteredSessionsList = filterConfirmedSessions(jsonData);
+renderSessionList(filteredSessionsList);
+
+$(document).on('click', '#sessionsList li.meeta-event-accordion-item > .meeta-event-accordion-toggle', function(){
+  if ($(this).hasClass("active")) {
+    $(this).removeClass("active");
+    $(this).siblings(".meeta-event-accordion-body").slideUp(200);
+} else {           
+    $(".meeta-event-accordion-item > .meeta-event-accordion-toggle").removeClass("active");
+    $(this).addClass("active");
+    $(".meeta-event-accordion-body").slideUp(200);
+    $(this).siblings(".meeta-event-accordion-body").slideDown(200);
+}
+});
