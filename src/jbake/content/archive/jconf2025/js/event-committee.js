240d7cf56dd8d45054e9e3dbcca0c58c32043853
Eudris Cabrera <eudris@gmail.com>
2025-12-05 18:02:44 -0400

    archiving 2025 website

---
+++ src/jbake/content/archive/jconf2025/js/event-committee.js
@@ -0,0 +1,37 @@
+import { getUsefulContents } from '/js/util-url.js';
+
+var fetchUrl = getUsefulContents("lang", "/archive/jconf2025/json/event-committee");
+
+fetch(fetchUrl)
+    .then(function (response) {
+        if (!response.ok) {
+            throw Error(response.statusText);
+        }
+
+        return response.json();
+    })
+    .then(function (membersJson) {
+        var members = document.getElementById('listMembers');
+        // traitement de l'objet
+        for (let i in membersJson) {
+            members.innerHTML += createMemberCard(membersJson[i]);
+        }
+});
+
+function createMemberCard(memberJson) {
+
+    var memberHtml = "<div class=\"col-lg-3\">" +
+     "<div class=\"single-speker-3\">" +
+     "<div class=\"speker-img\">" +
+     "<a href=\""+memberJson.socials[0].link+"\" target=\"_blank\"><img src=\"" +memberJson.photoUrl +"\" alt=\"" +memberJson.name +"\" title=\""+memberJson.shortBio +"\"/></a>" +
+     "<div class=\"speker-content text-center\">"+
+     "<h3 class=\"speaker-name\">"+memberJson.name+"</h3>"+
+     "<span class=\"speaker-designation\">" +memberJson.badges[0].description +" @ " +memberJson.company+"</span>" +
+     "</div>"+
+     "</div>"+
+     "</div>"+
+     "</div>";
+
+     return memberHtml;
+}
+
---
+++ src/jbake/content/archive/jconf2025/js/speakers.js
@@ -0,0 +1,41 @@
+import { getUsefulContents, getUsefulLink } from '/js/util-url.js';
+import {fetchData} from '/js/fetch-util.js';
+
+var fetchUrl = getUsefulContents("lang", "/archive/jconf2025/json/speakers");
+
+
+let createSpeakerCard = function(speakerJson) {
+
+    var speakerUrlDetail =  getUsefulLink("lang", "speaker-details.html?id=" + speakerJson.speakerId);
+
+      var speakerHtml = "<div class=\"col-lg-3\">" +
+      "<div class=\"single-speker-3\">" +
+      "<div class=\"speker-img\">" +
+      "<a href=\"" + speakerUrlDetail + "\"><img src=\"" +speakerJson.photoUrl +"\" alt=\"" +speakerJson.name +"\" /></a>"+
+      "<div class=\"speker-content text-center\">"+
+         "<h3 class=\"name\">" +speakerJson.name +"</h3>"+
+             "<p class=\"designation\">" +speakerJson.title +" @ " +speakerJson.company +" - " +speakerJson.country +" <span class=\"flag-icon "+speakerJson.countryFlag+"\"></span></p>" +
+      "</div>"+
+      "</div>"+
+      "</div>"+
+      "</div>";
+
+     return speakerHtml;
+}
+
+let renderSpeakerList = function(speakersList){
+         let speakers = document.getElementById('listSpeakers');
+         speakersList.sort(function(a, b){return a.numOrder-b.numOrder && b.featured - a.featured});
+         speakersList.forEach(speakerJson => speakers.innerHTML += createSpeakerCard(speakerJson));
+}
+
+let filterConfirmedSpeakers = function(speakersJson) {
+  let speakersList = [];
+  speakersJson.forEach(speakerObj => speakersList.push(speakerObj));
+  return speakersList.filter(item => item.display === true);
+}
+
+let jsonData = await fetchData(fetchUrl);
+let filteredSpeakersList = filterConfirmedSpeakers(jsonData);
+
+renderSpeakerList(filteredSpeakersList);
